f4304619eb29df42cbc5c10231e8bef153a2c95c
Adam Byrd <ajbyrd0723@gmail.com>
2020-01-13 14:19:10 -0600

    completes lists practice

--- chapter4lists.py
+++ chapter4lists.py
@@ -1,30 +1,30 @@
 # First list exercise
 
-# import random
+import random
 
-# my_randoms = list()
-# for i in range(10):
-#     my_randoms.append(random.randrange(1, 6))
+my_randoms = list()
+for i in range(10):
+    my_randoms.append(random.randrange(1, 6))
 
 
-# # Generate a list of numbers 1..10
-# numbers_1_to_10 = range(1, 11)
+# Generate a list of numbers 1..10
+numbers_1_to_10 = range(1, 11)
 
-# # Iterate from 1 to 10
-# for number in numbers_1_to_10:
-#     the_numbers_match = False
+# Iterate from 1 to 10
+for number in numbers_1_to_10:
+    the_numbers_match = False
 
-#     # Iterate your random number list here
-#     for randnumber in my_randoms:
-#         the_numbers_match = False
-#         if randnumber == number:
+    # Iterate your random number list here
+    for randnumber in my_randoms:
+        the_numbers_match = False
+        if randnumber == number:
 
-#             print(f'{number} is in the random list')
+            print(f'{number} is in the random list')
 
-#         else:
-#             print(f'{number} is not in the random list')
-#     # Does my_randoms contain number? Change the boolean.
+        else:
+            print(f'{number} is not in the random list')
+    # Does my_randoms contain number? Change the boolean.
 
-# Second list exercise
+
 
 
